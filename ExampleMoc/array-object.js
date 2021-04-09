d5f84ffd0d91141a5fc35d3e1607d4c1fd3441f8
Vyacheslav Salenok <vyacheslav.salenok@simbirsoft.com>
2021-04-09 14:23:48 +0400

    add moc

---
+++ ExampleMoc/array-object.js
@@ -0,0 +1,8 @@
+export const mocData = (count) =>
+    Array(12)
+        .fill(1)
+        .map((item, i) => ({
+            id: `211133232${i}`,
+            description: 'test description'
+        }));
+
